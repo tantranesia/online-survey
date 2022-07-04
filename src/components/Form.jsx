8c724cb697ba4a5ffa38ad208d45f3186cb3f8dd
tantranesia <tantranesia@gmail.com>
2022-07-04 23:17:13 +0700

    new update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { v4 as uuid } from 'uuid';
+
+// Material UI import
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import InputLabel from '@mui/material/InputLabel';
@@ -16,6 +20,7 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 
 function Form() {
+  const unique_id = uuid();
   const [question, setQuestion] = useState([]);
   const [input, setInput] = useState('');
   const [answer, setAnswer] = useState([]);
@@ -28,6 +33,7 @@ function Form() {
       answerText: '',
       answerDropdown: question,
       required: false,
+      id: unique_id
     },
   ]);
   // Add new option label
@@ -43,8 +49,9 @@ function Form() {
       answerText: '',
       answerDropdown: question,
       required: false,
+      id: unique_id
     };
-    setForm([ ...form , card]);
+    setForm([...form, card]);
     console.log(form);
   };
 
@@ -96,124 +103,175 @@ function Form() {
         </FormControl>
       </Box>
       {form.length > 0 ? (
-        form.map((data, index) => {
-          return (
-            <Box
-              component="form"
-              sx={{ bgcolor: 'white', padding: '30px', margin: '30px' }}
-              key={index}
-            >
-              <Stack spacing={5}>
-                <Stack direction="row" sx={{ justifyContent: 'space-between' }}>
-                  <TextareaAutosize
-                    name="question"
-                    placeholder="Your Question"
-                    variant="standard"
-                    style={{ width: 400 }}
-                    minRows={3}
-                    onChange={(e) =>
-                      setForm({ ...form, question: e.target.value, index })
-                    }
-                  />
-                  <FormControl sx={{ width: 300 }}>
-                    <InputLabel id="question-type">Please Choose</InputLabel>
-                    <Select
-                      labelId="question-type"
-                      id="question-type"
-                      label="question-type"
-                      onChange={changeType}
-                    >
-                      <MenuItem value={'dropdown'}>Dropdown</MenuItem>
-                      <MenuItem value={'text'}>Text</MenuItem>
-                    </Select>
-                  </FormControl>
-                </Stack>
-                {input === 'text' ? (
-                  <TextField
-                    name="answerText"
-                    label="Enter text"
-                    variant="standard"
-                    onChange={(e) =>
-                      setForm({ ...form, answerText: e.target.value, index })
-                    }
-                  ></TextField>
-                ) : question.length > 0 ? (
-                  question.map((col, index) => {
-                    return (
-                      <Stack
-                        direction="row"
-                        gap={10}
-                        sx={{ alignItems: 'center' }}
+        <DragDropContext>
+          <Droppable droppableId="cards">
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {form.map((data, index) => {
+                  return (
+                    <div>
+                      <Draggable
                         key={index}
+                        draggableId={data.id}
+                        index={index}
                       >
-                        <TextField
-                          label="Enter text"
-                          variant="standard"
-                          fullWidth
-                          onChange={optionAdd}
-                        />
-                        <IconButton onClick={addOption}>
-                          <AddCircleOutline
-                            sx={{ color: blue[700] }}
-                            fontSize="large"
-                          />
-                        </IconButton>
-                      </Stack>
-                    );
-                  })
-                ) : (
-                  <Stack
-                    direction="row"
-                    gap={10}
-                    sx={{ alignItems: 'center' }}
-                    // key={index}
-                  >
-                    <TextField
-                      label="Enter text"
-                      variant="standard"
-                      fullWidth
-                      onChange={optionAdd}
-                    />
-                    <IconButton onClick={addOption}>
-                      <AddCircleOutline
-                        sx={{ color: blue[700] }}
-                        fontSize="large"
-                      />
-                    </IconButton>
-                  </Stack>
-                )}
-              </Stack>
-              <Stack
-                direction="row"
-                gap={2}
-                sx={{
-                  justifyContent: 'flex-end',
-                  alignItems: 'center',
-                  marginTop: '20px',
-                }}
-              >
-                <IconButton>
-                  <AddCircleOutline
-                    fontSize="large"
-                    sx={{ color: blue[700] }}
-                    onClick={addCard}
-                  />
-                </IconButton>
-                <Divider orientation="vertical" variant="middle" flexItem />
-                <FormGroup>
-                  <FormControlLabel
-                    name="required"
-                    control={<Switch checked={form.required} />}
-                    label="Required"
-                    onChange={(e) =>
-                      setForm({ ...form, required: e.target.value, index })
-                    }
-                  />
-                </FormGroup>
-              </Stack>
-            </Box>
-          );
-        })
+                        {(provided) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            <Box
+                              component="form"
+                              sx={{
+                                bgcolor: 'white',
+                                padding: '30px',
+                                margin: '30px',
+                              }}
+                             
+                            >
+                              <Stack spacing={5}>
+                                <Stack
+                                  direction="row"
+                                  sx={{ justifyContent: 'space-between' }}
+                                >
+                                  <TextareaAutosize
+                                    name="question"
+                                    placeholder="Your Question"
+                                    variant="standard"
+                                    style={{ width: 400 }}
+                                    minRows={3}
+                                    onChange={(e) =>
+                                      setForm({
+                                        ...form,
+                                        question: e.target.value,
+                                        index,
+                                      })
+                                    }
+                                  />
+                                  <FormControl sx={{ width: 300 }}>
+                                    <InputLabel id="question-type">
+                                      Please Choose
+                                    </InputLabel>
+                                    <Select
+                                      labelId="question-type"
+                                      id="question-type"
+                                      label="question-type"
+                                      onChange={changeType}
+                                    >
+                                      <MenuItem value={'dropdown'}>
+                                        Dropdown
+                                      </MenuItem>
+                                      <MenuItem value={'text'}>Text</MenuItem>
+                                    </Select>
+                                  </FormControl>
+                                </Stack>
+                                {input === 'text' ? (
+                                  <TextField
+                                    name="answerText"
+                                    label="Enter text"
+                                    variant="standard"
+                                    onChange={(e) =>
+                                      setForm({
+                                        ...form,
+                                        answerText: e.target.value,
+                                        index,
+                                      })
+                                    }
+                                  ></TextField>
+                                ) : question.length > 0 ? (
+                                  question.map((col, index) => {
+                                    return (
+                                      <Stack
+                                        direction="row"
+                                        gap={10}
+                                        sx={{ alignItems: 'center' }}
+                                        key={index}
+                                      >
+                                        <TextField
+                                          label="Enter text"
+                                          variant="standard"
+                                          fullWidth
+                                          onChange={optionAdd}
+                                        />
+                                        <IconButton onClick={addOption}>
+                                          <AddCircleOutline
+                                            sx={{ color: blue[700] }}
+                                            fontSize="large"
+                                          />
+                                        </IconButton>
+                                      </Stack>
+                                    );
+                                  })
+                                ) : (
+                                  <Stack
+                                    direction="row"
+                                    gap={10}
+                                    sx={{ alignItems: 'center' }}
+                                    // key={index}
+                                  >
+                                    <TextField
+                                      label="Enter text"
+                                      variant="standard"
+                                      fullWidth
+                                      onChange={optionAdd}
+                                    />
+                                    <IconButton onClick={addOption}>
+                                      <AddCircleOutline
+                                        sx={{ color: blue[700] }}
+                                        fontSize="large"
+                                      />
+                                    </IconButton>
+                                  </Stack>
+                                )}
+                              </Stack>
+                              <Stack
+                                direction="row"
+                                gap={2}
+                                sx={{
+                                  justifyContent: 'flex-end',
+                                  alignItems: 'center',
+                                  marginTop: '20px',
+                                }}
+                              >
+                                <IconButton>
+                                  <AddCircleOutline
+                                    fontSize="large"
+                                    sx={{ color: blue[700] }}
+                                    onClick={addCard}
+                                  />
+                                </IconButton>
+                                <Divider
+                                  orientation="vertical"
+                                  variant="middle"
+                                  flexItem
+                                />
+                                <FormGroup>
+                                  <FormControlLabel
+                                    name="required"
+                                    control={<Switch checked={form.required} />}
+                                    label="Required"
+                                    onChange={(e) =>
+                                      setForm({
+                                        ...form,
+                                        required: e.target.value,
+                                        index,
+                                      })
+                                    }
+                                  />
+                                </FormGroup>
+                              </Stack>
+                            </Box>
+                          </div>
+                        )}
+                      </Draggable>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       ) : (
         <Box component="form" sx={{ bgcolor: 'white', padding: '30px' }}>
           <Stack spacing={5}>
